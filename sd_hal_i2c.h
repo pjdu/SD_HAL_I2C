311c36e18d207cffdf407ec9b2ab5b8be994dd6f
Sina Darvishi <sinadarvi@gmail.com>
2016-07-24 19:01:49 +0430

    Update sd_hal_i2c.h

--- sd_hal_i2c.h
+++ sd_hal_i2c.h
@@ -16,6 +16,9 @@ extern "C" {
 
 
 #include "stm32f0xx_hal.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
  /**
  * @}
@@ -61,117 +64,35 @@ typedef enum {
  * @param  device_address: 7-bit, left aligned device address used for communication
  * @retval One of @ref SD_I2C_Result enumeration
  */
-SD_I2C_Result SD_I2C_IsDeviceConnected(I2C_HandleTypeDef* I2Cx, uint8_t address);
 
-/**
- * @brief  Check which error has occurred
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @retval One of @ref SD_I2C_Result enumeration
- */
 SD_I2C_Result SD_I2C_CheckError(I2C_HandleTypeDef* I2Cx);
-
-/**
- * @brief  Writes single byte to device
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address where you want to write data
- * @param  data: Data to be written to device
- * @retval Member of @ref SD_I2C_Result enumeration
- */
+SD_I2C_Result SD_I2C_IsDeviceConnected(I2C_HandleTypeDef* I2Cx, uint8_t address);
+SD_I2C_Result SD_I2C_WriteBit(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t bitNum, uint8_t data);
+SD_I2C_Result SD_I2C_WriteBitW(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t bitNum,uint16_t data);
+SD_I2C_Result SD_I2C_WriteBits(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint16_t register_address, uint8_t bitStart, uint8_t length,uint8_t data);
+SD_I2C_Result SD_I2C_WriteBitsW(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint16_t register_address, uint8_t bitStart, uint8_t length,uint16_t data);
 SD_I2C_Result SD_I2C_Write(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t data);
-
-/**
- * @brief  Writes multiple data to device
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address where data will be written
- * @param  *data: Data to be written to device.
- * @param  count: Number of elements to write starting at register register_address
- * @retval One of @ref SD_I2C_Result enumeration
- */
-SD_I2C_Result SD_I2C_WriteSome(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint16_t register_address, uint8_t *data, uint16_t count);
-
-/**
- * @brief  Writes single byte to device without specifying register address, can be used for command write
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  data: Data to be written to device
- * @retval One of @ref SD_I2C_Result enumeration
- */
+SD_I2C_Result SD_I2C_WriteSome(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint16_t register_address, uint16_t count, uint8_t *data);
+SD_I2C_Result SD_I2C_WriteByte(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t data);
+SD_I2C_Result SD_I2C_WriteBytes(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t count, uint8_t *data);
+SD_I2C_Result SD_I2C_WriteWord(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address,uint16_t data);
+SD_I2C_Result SD_I2C_WriteWords(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t length,uint16_t *data);
 SD_I2C_Result SD_I2C_WriteWithNoRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t data);
-
-/**
- * @brief  Writes multiple data to device without register address
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  *data: Pointer to data array to be written to device. Array length is the same as number of elements you want to write
- * @param  count: Number of elements to write
- * @retval One of @ref SD_I2C_Result enumeration
- */
-SD_I2C_Result SD_I2C_WriteSomeWithNoRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t* data, uint16_t count);
-
-/**
- * @brief  Writes single byte in a 16-bit length register address
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address where data will be written
- * @param  data: Data byte to write
- * @retval One of @ref SD_I2C_Result enumeration
- */
+SD_I2C_Result SD_I2C_WriteMultiWithNoRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t* data, uint16_t count);
 SD_I2C_Result SD_I2C_WriteWith16BitRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint16_t register_address, uint8_t data);
-
-/**
- * @brief  Reads single byte from device
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address from where read will be done
- * @param  *data: Pointer to variable where data will be stored from read operation
- * @retval One of @ref SD_I2C_Result enumeration
- */
-SD_I2C_Result SD_I2C_Read(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t* data);
-
-/**
- * @brief  Reads multiple bytes from device
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address from where read operation will start
- * @param  *data: Pointer to variable where data will be stored from read operation
- * @param  count: Number of elements to read from device
- * @retval One of @ref SD_I2C_Result enumeration
- */
-SD_I2C_Result SD_I2C_ReadSome(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t* data, uint16_t count);
-
-/**
- * @brief  Reads I2C data without specifying register address
- * @note   This can be used if your sensors just sends data, without any registers
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  *data: Pointer to variable where data will be stored from read operation
- * @retval One of @ref SD_I2C_Result enumeration
- */
+SD_I2C_Result SD_I2C_ReadBit(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t bitNum, uint8_t *data);
+SD_I2C_Result SD_I2C_ReadBitW(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t bitNum,uint16_t *data);
+SD_I2C_Result SD_I2C_ReadBits(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint16_t register_address, uint8_t bitStart, uint8_t length,uint8_t *data);
+SD_I2C_Result SD_I2C_ReadBitsW(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint16_t register_address, uint8_t bitStart, uint8_t length,uint16_t *data);
+SD_I2C_Result SD_I2C_Read(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t *data);
+SD_I2C_Result SD_I2C_ReadSome(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint16_t count, uint8_t *data);
+SD_I2C_Result SD_I2C_ReadByte(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t *data);
+SD_I2C_Result SD_I2C_ReadBytes(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t register_address, uint8_t count, uint8_t *data);
+SD_I2C_Result SD_I2C_ReadWord(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address,uint16_t *data);
+SD_I2C_Result SD_I2C_ReadWords(I2C_HandleTypeDef* I2Cx,uint8_t device_address, uint8_t register_address, uint8_t length,uint16_t *data);
 SD_I2C_Result SD_I2C_ReadWithNoRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t* data);
-
-/**
- * @brief  Reads multiple bytes from device without specifying register address
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  *data: Pointer to variable where data will be stored from read operation
- * @param  count: Number of elements to read from device
- * @retval One of @ref SD_I2C_Result enumeration
- */
 SD_I2C_Result SD_I2C_ReadSomeWithNoRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint8_t* data, uint16_t count);
-
-/**
- * @brief  Reads single byte from device with 16-bit register address
- * @param  *I2Cx: Pointer to I2Cx peripheral to be used in communication
- * @param  device_address: 7-bit, left aligned device address used for communication
- * @param  register_address: Register address from where read will be done
- * @param  *data: Pointer to variable where data will be stored from read operation
- * @retval One of @ref SD_I2C_Result enumeration
- */
 SD_I2C_Result SD_I2C_ReadWith16BitRegisterAddress(I2C_HandleTypeDef* I2Cx, uint8_t device_address, uint16_t register_address, uint8_t* data);
-
-
 /**
  * @}
  */
